241cb7e88af8f2bb09c20532f428bb7f3741ca36
Weston Renoud <weston.renoud@qps.nl>
2022-12-14 21:17:41 +0100

    day 9 in progress

--- day_09.py
+++ day_09.py
@@ -1,8 +1,28 @@
-from util import Test, Answer, ReadPuzzle, ReadExamplePuzzle
+from util import Test, Answer, ReadPuzzle, ReadExamplePuzzle, Point
 
 
 def part1(data):
-    return None
+    h = Point(0,0)
+    t = Point(0,0)
+    trail = set((t,))
+    for line in data:
+        dir, count = line.split()
+        if dir == 'R': step = Point(1,0)
+        elif dir == 'L': step = Point(-1,0)
+        elif dir == 'U': step = Point(0,1)
+        elif dir == 'D': step = Point(0,-1)
+        for i in range(int(count)):
+            next_h = h + step
+            if h != t and next_h != t:
+                delta = next_h-t
+                if delta.x == 0 or delta.y == 0:
+                    t += step
+                elif abs(delta.x) == 2 or abs(delta.y) == 2:
+                    t=h
+            trail.add(t)
+            h = next_h
+            print(h,t)
+    return len(trail)
 
 
 def part2(data):
