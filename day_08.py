fc2ac5874704f4636c0641a598e272b926854da8
Weston Renoud <weston.renoud@qps.nl>
2022-12-15 15:25:32 +0100

    day 8 partial progress

--- day_08.py
+++ day_08.py
@@ -2,7 +2,28 @@ from util import Test, Answer, ReadPuzzle, ReadExamplePuzzle
 
 
 def part1(data):
-    return None
+    res = []
+    rows = len(data)
+    cols = len(data[0])
+    for _ in range(rows):
+        res.append([False] * cols)
+
+    # down
+    for c in range(cols):
+        for r in range(rows - 1):
+            if int(data[r][c]) < int(data[r + 1][c]):
+                res[r][c] = True
+    # up
+    for c in range(cols):
+        for r in range(rows - 1, 0, -1):
+            if int(data[r][c]) < int(data[r - 1][c]):
+                res[r][c] = True
+
+    # left
+    for r in range(rows):
+        for c in range(cols - 1)
+
+    return sum(sum(c for c in row) for row in res)
 
 
 def part2(data):
